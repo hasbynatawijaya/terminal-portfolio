0e6f0e5db2fbd651ca8ef2e0502624d11c3a40c5
hasbynatawijaya <mhasbyn@gmail.com>
2025-04-17 15:46:02 +0700

    add github pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,58 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.ts",
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  const repositoryName = "terminal-portfolio";
+  const publicPath = isProduction ? `/${repositoryName}/` : "/";
+
+  return {
+    entry: "./src/index.ts",
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: publicPath,
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "bundle.css",
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "index.html",
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "bundle.css",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-    }),
-  ],
-  devServer: {
-    static: "./dist",
-    port: 8080,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-  },
-  mode: "development",
+    devServer: {
+      static: "./dist",
+      port: 8080,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+      devMiddleware: {
+        publicPath: "/",
+      },
+    },
+    mode: isProduction ? "production" : "development",
+  };
 };
